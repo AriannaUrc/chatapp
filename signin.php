e37fd9ad85968a0d0d5038f92d31dc90e268035a
URCIUOLI ARIANNA <URCIUOLI.22381>
2024-12-03 11:06:48 +0100

    login front end

--- signin.php
+++ signin.php
@@ -16,29 +16,30 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  
+    <link rel="stylesheet" type="text/css" href="css/signin.css">
+
     <div class="signin-form">
         <form action="" method="post">
             <div class="form-header">
                 <h2>Sign in</h2>
                 <p>Login to MyChat</p>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" name="email" placeholder="someone@site.com" autocomplete="off" Required>
-                </div>
-
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" name="password" placeholder="Password" autocomplete="off" Required>
-                </div>
-
-                <div class="small">Forgot password? <a href="forgot_pass.php">Click here</a></div>
-
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-block btn-lg">Sign in</button>
-                </div>
-                <!-- <?php //include("signin_user.php")?> -->
             </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" name="email" placeholder="someone@site.com" autocomplete="off" Required>
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" name="password" placeholder="Password" autocomplete="off" Required>
+            </div>
+
+            <div class="small">Forgot password? <a href="forgot_pass.php">Click here</a></div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary btn-block btn-lg">Sign in</button>
+            </div>
+            <!-- <?php //include("signin_user.php")?> -->
         </form>
 
         <div class="text-center small" style="color: #67428B;"> Don't have an account? <a href="signup.php">Create one</a></div>
